47e185f5e3876a66ad0e63f151a64923feb1e0ea
Bruno De Bondt <mail@brunodbo.be>
2013-10-25 11:05:17 -0700

    Verifying route json data

--- js/parks.js
+++ js/parks.js
@@ -1,19 +1,18 @@
-$(document).ready(function() {
+$(function() {
 
-var parks_layer = L.geoJson(null, {
-        style: {
-            color: '#666',
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.3
-        }
-    });
+  var parks_layer = L.geoJson(null, {
+    style: {
+      color: '#666',
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.3
+    }
+  });
 
-$.getJSON('js/bc_parks_ecores_topo.json', function (data) {
-    var parks_geojson = topojson.feature(data, data.objects.bc_parks_ecores);
-    parks_layer.addData(parks_geojson);
-});
+  $.getJSON('js/parks_data.json', function (data) {
+      var parks_geojson = topojson.feature(data, data.objects.bc_parks_ecores);
+      parks_layer.addData(parks_geojson);
+  });
 
-map.addLayer(parks_layer);
-
+  map.addLayer(parks_layer);
 });
