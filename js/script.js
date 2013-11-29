fa978f758393ce698ddd1767c03da2b7bdcfbe34
Bruno De Bondt <mail@brunodbo.be>
2013-11-28 18:35:06 -0800

    Some basic styling

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ if ($(window).width() < 626) {
 } else {
   var map = L.mapbox.map('map', 'brunodbo.map-8dbz5o8v', {
     legendControl: {
-      position: 'bottomleft'
+      position: 'bottomright'
+    },
+    attributionControl: {
+      position: 'bottomright'
     }
   }).setView([54, -116.5], 6);
 }
